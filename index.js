e42831ff596e83f98da974bc93f7db5b4a406838
DarkSpaceman <111444946+DarkSpaceman@users.noreply.github.com>
2022-12-14 20:35:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+#! usr/bin/env
+const readline = require('readline')
+const fs = require('fs');
+const process = require('process')
+
+const inquirer = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let win = 0;
+let lose = 0
+
+const s = process.argv;
+const gameFile = s[s.length - 1];
+
+fs.appendFile(gameFile, '', (err) => { if (err) throw err })
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+  }
+
+function eagle() {
+        inquirer.question('Орёл илл Решка? (1/2) ', (guess) => {
+            if (guess == 'exit') { process.exit() };
+            
+            let correctAnswer = getRandomIntInclusive(1, 2);
+
+            if (guess == correctAnswer) {
+                win += 1
+                console.log(`Exelent! The correct answer is ${correctAnswer}.`);
+                console.log(`You win ${win} and lose ${lose}`);
+                fs.writeFile(gameFile, `You win ${win} and lose ${lose}`, (err) => {
+                  if (err) throw err
+                });
+                eagle();
+            }
+            else if (guess != correctAnswer) {
+                lose += 1;
+                console.log(`You were so close! The correct answer is ${correctAnswer}.`);
+                console.log(`You win ${win} and lose ${lose}`);
+                fs.writeFile(gameFile, `You win ${win} and lose ${lose}`, (err) => {
+                  if (err) throw err
+                });
+                eagle();
+            };
+        });
+        
+}
+
+eagle();
